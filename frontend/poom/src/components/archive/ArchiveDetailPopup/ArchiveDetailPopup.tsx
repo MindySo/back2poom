3980ead7b5ef5fbb5b4eb1958438055a63488370
honeybutter7 <honeybutter7@naver.com>
2025-11-30 01:24:54 +0900

    feat(S13P31A706-154): AI서포트 이미지 뷰포트 하단 문구 수정

--- frontend/poom/src/components/archive/ArchiveDetailPopup/ArchiveDetailPopup.tsx
+++ frontend/poom/src/components/archive/ArchiveDetailPopup/ArchiveDetailPopup.tsx
@@ -637,7 +637,7 @@ const ArchiveDetailPopup: React.FC<ArchiveDetailPopupProps> = ({ personId, initi
               onClick={(e) => e.stopPropagation()}
               onTouchEnd={(e) => e.stopPropagation()}
             >
-              {(aiImageZoom * 100).toFixed(0)}% | 스크롤/핀치로 확대/축소
+              {(aiImageZoom * 100).toFixed(0)}%
             </div>
           </div>
         ) : null;
